f31bf68f08767184e2b3df693ea8961751d7e0e7
Swathi <swathi.k@indianeagle.com>
2020-01-13 23:29:50 -0600

    hello

---
+++ src/main/java/com/indianeagle/internal/MySQLConfig.java
@@ -0,0 +1,28 @@
+package com.indianeagle.internal;
+
+import io.micrometer.core.instrument.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+class MySQLConfig {
+
+    private static final String ENV = StringUtils.isNotBlank(System.getenv("ENV_SYSTEM")) ? System.getenv("ENV_SYSTEM") :(StringUtils.isNotBlank(System.getProperty("ENV_SYSTEM")) ? System.getProperty("ENV_SYSTEM") : "dev");
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    DataSource mySQLDataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(environment.getProperty("${ENV}.internal.datasource.url"));
+        driverManagerDataSource.setUsername(environment.getProperty("${ENV}.internal.datasource.username"));
+        driverManagerDataSource.setPassword(environment.getProperty("${ENV}.internal.datasource.password"));
+        driverManagerDataSource.setDriverClassName(environment.getProperty("${ENV}.internal.datasource.driver-class-name"));
+       return driverManagerDataSource;
+    }
+
+}
---
+++ src/test/java/com/indianeagle/internal/InternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indianeagle.internal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
